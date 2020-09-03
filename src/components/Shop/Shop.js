95e7f862a786d4d21cdcde252876144d6962f9ab
Mainuddinhasansakib <sakibuddin831@gmail.com>
2020-09-03 11:53:01 +0600

    38.4 completed

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,29 +1,39 @@
-
-
-import React from 'react';
+import React, { useEffect } from 'react';
 import fakeData from '../../fakeData';
 import { useState } from "react";
 import './Shop.css'
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
-import { addToDatabaseCart } from '../../utilities/databaseManager';
+import { addToDatabaseCart, getDatabaseCart } from '../../utilities/databaseManager';
 
 const Shop = () => {
   const first10 = fakeData.slice(0,10);
   const [products,setProducts] = useState(first10);
   const[cart,setCart] = useState([])
-
+ 
+useEffect(() =>{
+  const savedCart = getDatabaseCart();
+  // console.log(savedCart);
+  const productKeys = Object.keys(savedCart);
+  // console.log(productKeys)
+  const previousCart = productKeys.map (existingKey => {
+  const product = fakeData.find(pd =>pd.key === existingKey);
+  product.quantity = savedCart[existingKey]
+  // console.log(existingKey,savedCart[existingKey])
+  return product;
+  })
+  setCart(previousCart)
+// console.log(savedCart); 
+console.log(previousCart) 
+},[])
   const handleAddProduct = (product) => {
    
     const toBeAddedKey = product.key
     const sameProduct = cart.find(pd => pd.key === toBeAddedKey )
     let count = 1;
     let newCart;
-    // let count sameProduct.quantity =count;
-  // }
     if(sameProduct){
-      const count = sameProduct.quantity + 1;//sameProducter notun count
-      // sameProduct.quantity = sameProduct.quantity + 1;
+      const count = sameProduct.quantity + 1;
       sameProduct.quantity = count;
       const others = cart.filter(pd => pd.key !== toBeAddedKey)
       newCart = [...others, sameProduct]
@@ -32,18 +42,9 @@ const Shop = () => {
       product.quantity = 1;
       newCart = [...cart, product]
     }
-//     // const count = sameProduct.length
 
-//     // const sameProduct = newCart.filter(pd => pd.key === product.key)
-//     // const count = sameProduct.length
-//     // console.log('Product added',product)
-
-//     // const newCart = [...cart,product];
 setCart(newCart);
-//     // const sameProduct = newCart.filter(pd => pd.key === product.key)
-//  const count = sameProduct.length
-
- addToDatabaseCart (product.key,count)
+addToDatabaseCart (product.key,count)
  }
 
 return (
@@ -69,6 +70,79 @@ return (
 );
 };
 export default Shop;
+
+
+// 38.4
+// import React, { useEffect } from 'react';
+// import fakeData from '../../fakeData';
+// import { useState } from "react";
+// import './Shop.css'
+// import Product from '../Product/Product';
+// import Cart from '../Cart/Cart';
+// import { addToDatabaseCart } from '../../utilities/databaseManager';
+
+// const Shop = () => {
+//   const first10 = fakeData.slice(0,10);
+//   const [products,setProducts] = useState(first10);
+//   const[cart,setCart] = useState([])
+
+
+//   const handleAddProduct = (product) => {
+   
+//     const toBeAddedKey = product.key
+//     const sameProduct = cart.find(pd => pd.key === toBeAddedKey )
+//     let count = 1;
+//     let newCart;
+//     // let count sameProduct.quantity =count;
+//   // }
+//     if(sameProduct){
+//       const count = sameProduct.quantity + 1;//sameProducter notun count
+//       // sameProduct.quantity = sameProduct.quantity + 1;
+//       sameProduct.quantity = count;
+//       const others = cart.filter(pd => pd.key !== toBeAddedKey)
+//       newCart = [...others, sameProduct]
+//     }
+//     else{
+//       product.quantity = 1;
+//       newCart = [...cart, product]
+//     }
+// //     // const count = sameProduct.length
+
+// //     // const sameProduct = newCart.filter(pd => pd.key === product.key)
+// //     // const count = sameProduct.length
+// //     // console.log('Product added',product)
+
+// //     // const newCart = [...cart,product];
+// setCart(newCart);
+// //     // const sameProduct = newCart.filter(pd => pd.key === product.key)
+// //  const count = sameProduct.length
+
+//  addToDatabaseCart (product.key,count)
+//  }
+
+// return (
+// <div className="twin-container">
+//     <div className="product-container">
+//     {
+//     products.map(pd => <Product
+//         key={pd.key}
+//         showAddToCart = {true}  
+//         handleAddProduct = { handleAddProduct}
+//         produces ={pd}>
+
+//          </Product> )  
+//     }
+    
+//     </div>
+//     <div className="cart-container">
+//        <Cart cart= {cart}></Cart>
+//     </div>
+    
+// </div>
+
+// );
+// };
+// export default Shop;
 
 
 // 37.7
